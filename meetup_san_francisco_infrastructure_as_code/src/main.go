fbf57738267cf53c5f730862a8b78e5d660c64ba
zane <zane.williamson@gmail.com>
2016-10-06 23:36:30 -0700

    adding ec2 metadata lookup and print ami-id

--- meetup_san_francisco_infrastructure_as_code/src/main.go
+++ meetup_san_francisco_infrastructure_as_code/src/main.go
@@ -5,14 +5,29 @@ import (
     "net/http"
     "os/exec"
     "log"
+    "github.com/aws/aws-sdk-go/aws/client"
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/aws/ec2metadata"
 )
+
+type EC2Metadata struct {
+   *client.Client
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
     out, err := exec.Command("/bin/hostname", "-f").Output()
     if err != nil {
         log.Fatal(err)
     }
+    sess, err := session.NewSession()
+    svc := ec2metadata.New(sess)
+    resp, err := svc.GetMetadata("ami-id")
+    if err != nil {
+	    fmt.Println(err)
+	    return
+    }
     fmt.Fprintf(w, "AWS EC2 instance: %s", out)
+    fmt.Fprintf(w, "AWS AMI: %s", resp)
 }
 
 func main() {
