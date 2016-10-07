f36de92072f76465f476d22cd54cb33022cfc336
zane <zane.williamson@gmail.com>
2016-10-06 22:48:30 -0700

    updating to use ami data source

--- meetup_san_francisco_infrastructure_as_code/terraform/asg.tf
+++ meetup_san_francisco_infrastructure_as_code/terraform/asg.tf
@@ -4,7 +4,7 @@ module "asg" {
     vpc_zone_subnets    = "subnet-f05fdda8,subnet-2b82b64f,subnet-fc703f8a"
     security_groups     = "{aws_security_group.allow_goapp.id}"
     instance_type       = "t2.micro"
-    ami                 = "${var.ami}"
+    ami                 = "${data.aws_ami.goapp.id}"
     key_name            = "autozane"
     user_data           = "userdata.sh"
     asg_min_instances   = "1"
@@ -14,4 +14,15 @@ module "asg" {
     availability_zones  = "us-west-2a,us-west-2b,us-west-2c"
     environment         = "dev"
     team                = "autozane"
+}
+
+data "aws_ami" "goapp" {  
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["go-hostname-app-*"]
+  }
+
+  owners = ["self"]
 }
